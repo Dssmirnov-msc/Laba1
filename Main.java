e78f93c12bb19a09ead818eb1cc2490a62a08360
meow-error <egorlevadniy2002@gmail.com>
2022-02-19 14:12:04 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Рабы не мы, мы не рыбы AD");
+        System.out.println("Рабы не мы, мы не рыбы ADEI");
         System.out.println("Левадний");
         System.out.println("Зорина");
         System.out.println("Гирман");
