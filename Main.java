b3ab7d4ba0f79d9f38e19a75f01aa8b0b71f2a8c
dash1ne1dash <dasha.days24@gmail.com>
2022-02-19 14:10:00 +0300

    add AD (предыдущий раз забыла :))

--- Main.java
+++ Main.java
@@ -5,18 +5,11 @@
  */
 package laba1;
 
-/**
- *
- * @author Dmitry
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Рабы не мы, мы не рыбы");
+        System.out.println("Рабы не мы, мы не рыбы AD");
         System.out.println("Левадний");
         System.out.println("Зорина");
         System.out.println("Гирман");
