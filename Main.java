3881550346a53e654043261305deb77b7eb9a0d9
Basty64 <88938330+Basty64@users.noreply.github.com>
2022-02-19 14:13:38 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println("Левадний");
         System.out.println("Зорина");
         System.out.println("Гирман");
+        System.out.println("Бондаренко");
     }
     
 }
