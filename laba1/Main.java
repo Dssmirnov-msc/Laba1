4b4a342d8af60eb5cc776bfa70be84e141edab31
MariaRumyantseva <100023469+MariaRumyantseva@users.noreply.github.com>
2022-02-26 12:19:10 +0300

    Create Main.java
    внесены изменения

---
+++ laba1/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laba1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+<<<<<<< HEAD:Main.java
+        System.out.println("Рабы не мы, мы не рыбы, М.А.");
+        System.out.println("Левадний");
+        System.out.println("Зорина");
+        System.out.println("Гирман");
+        System.out.println("Румянцева");
+=======
+        System.out.println("Рабы не мы, мы не рыбы ADEI...DAVSRL");
+        System.out.println("Левадний");
+        System.out.println("Зорина");
+        System.out.println("Гирман");
+        System.out.println("Бондаренко");
+        System.out.println("Мичурина");
+        System.out.println("Романенков");
+        System.out.println("Кузнецов");
+        System.out.println("Саломатина");
+        System.out.println("Озказанч");
+        System.out.println("Румянцева");
+>>>>>>> 91959ee0fb01cbd170b78ce5c11f8300483a1318:laba1/Main.java
+    }
+    
+}
