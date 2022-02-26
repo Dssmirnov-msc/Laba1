91959ee0fb01cbd170b78ce5c11f8300483a1318
Eliz2001 <100404910+Eliz2001@users.noreply.github.com>
2022-02-26 11:44:14 +0300

    Update Main.java

--- laba1/Main.java
+++ laba1/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("Романенков");
         System.out.println("Кузнецов");
         System.out.println("Саломатина");
+        System.out.println("Озказанч");
     }
     
 }
